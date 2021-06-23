fd0eaa4f95761f27ded41ab8ac90b5f592dc5e29
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 17:28:39 +0530

    Initial Commit

---
+++ SwiftUI-SpicyDishes/SwiftUI_SpicyDishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SpicyDishesApp.swift
+//  SwiftUI-SpicyDishes
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SpicyDishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
